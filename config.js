be9ee6a3cfc74fc2828495bcc27db36413bc1c1c
Srajan Saxena <srajan.saxena7@gmail.com>
2024-07-04 11:49:33 +0530

    new mongodburl added

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-const mongodbURL = 'mongodb+srv://srajansaxena7:ARINrQgUdQe1RIrG@moodsapp.3pm36km.mongodb.net/?retryWrites=true&w=majority&appName=MoodsApp'
+const mongodbURL = 'mongodb+srv://moods_user:moods181035@moodsapp.3pm36km.mongodb.net/?retryWrites=true&w=majority&appName=MoodsApp'
 
 export default mongodbURL;
